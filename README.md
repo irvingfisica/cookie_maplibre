f49b16f691ae5bfe465f9778277d4d103b3f4d19
Irving Morales <irvingfisica@gmail.com>
2024-11-21 11:31:58 -0600

    creación de plantilla
    Primera versión de la plantilla

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# cookie_maplibre
- Plantilla para mapas con maplibre
+Template para generar una web para dataviz
+====================
+
+Este es un template para generar una web para mapas usando:
+- [Protomaps](https://protomaps.com/)
+- [D3js](https://d3js.org/)
+- [MapLibre](https://maplibre.org/)
+- [Topojson](https://github.com/topojson/topojson)
+- [Bootstrap](https://getbootstrap.com/)
+
+Requerimientos
+------------
+Python
+
+Instalar `cookiecutter`
+
+desde la línea de comandos: `pip install cookiecutter`    
+
+Uso
+-----
+Para crear un proyecto de web nuevo:
+
+`cookiecutter gh:irvingfisica/cookie_maplibre`
+
+Recomendaciones
+-----
+Para poder visualizar el mapa en local es necesario tener un servidor. Si usas VSCode para editar instala la extensión liveserver aunque esta puede tener problemas para servir los tiles.
+
+Es necesario un servidor capaz de resolver peticiones por rangos para servir los tiles. La mejor manera es montar uno a través de python con [RangeHTTPServer](https://github.com/danvk/RangeHTTPServer)
+
+Una vez instalado este servidor para ejecutarlo basta con teclear en la terminal:
+
+`python -m RangeHTTPServer`
+
+Tambien es necesario un mapabase. Este puede conseguirse desde mapbox, cartodb o incluso openstreetmap. Sin embargo usualmente hay un mapa base en la carpeta de tiles donde usualmente se usa esta plantilla. Este mapa base se generó con protomaps.
---
+++ {{cookiecutter.nombre}}/js/script.js
@@ -0,0 +1,87 @@
+let width = parseInt(d3.select("#mapa").style("width"));
+let height = window.innerWidth > 576 ? width*0.56 : width*1.3;
+
+d3.select("#mapa").style("height",height + "px");
+let caja_mapa = document.getElementById('mapa');
+
+const center = [24.433333, -102.133333];
+const zoom = 5;
+
+const protocol = new pmtiles.Protocol();
+maplibregl.addProtocol('pmtiles', protocol.tile);
+
+const PMTILES_URL = '../ts/mexbase.pmtiles';
+const ftiles = '../ts/fl'
+
+const p = new pmtiles.PMTiles(PMTILES_URL);
+protocol.add(p);
+
+let stl = [d3.json("./resources/datos/light.json")];
+
+Promise.all(stl).then(function(data){
+
+    let style = data[0];
+    style["sources"] = {
+        "protomaps": {
+            "type": 'vector',
+            "url": `pmtiles://${PMTILES_URL}`,
+            "attribution": '© <a href="https://morlan.mx">MORLAN</a>'
+        }
+    };
+
+    let mbase = {{cookiecutter.data}};
+    if (mbase == "OSM"){
+        style = {
+            "version": 8,
+            "sources": {
+              "osm": {
+                "type": "raster",
+                "tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+                "tileSize": 256,
+                "attribution": "&copy; OpenStreetMap Contributors",
+                "maxzoom": 19
+              }
+            },
+            "layers": [
+              {
+                "id": "osm",
+                "type": "raster",
+                "source": "osm"
+              }
+            ]
+          }
+    }
+
+
+    const map = new maplibregl.Map({
+        container: 'mapa',
+        zoom: zoom,
+        center: center,
+        style: style
+    });
+
+    map.on("load", () => {
+        map.addSource('full',{
+            type: "vector",
+    
+            url: "pmtiles://" + ftiles,
+            attribution: '© <a href="https://www.inegi.org.mx/">INEGI</a>'
+        });
+    
+        map.addLayer({
+            "id":"manzanas",
+            "source": "full",
+            "source-layer":"manzanas",
+            "type": "fill",
+            "paint": {
+                "fill-color": "red",
+                "fill-outline-color": "black",
+                "fill-opacity": 0.3
+            }
+        });
+
+    });
+
+});
+
+
